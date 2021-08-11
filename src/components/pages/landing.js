c28e6868bc3da85f6629376f82c1d7d6b0b25f00
alexjones <ajonzy@gmail.com>
2021-08-10 20:46:34 -0600

    Initial styles for landing page

--- src/components/pages/landing.js
+++ src/components/pages/landing.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import logo from "../../../static/assets/Libarbarian.jpg"
+
 export default function landing() {
     return (
         <div className='landing-wrapper'>
-            <h1>Welcome to Libarbarian</h1>
-            <img src="" alt="Logo"/>
+            <h1>Welcome to<br/>Libarbarian</h1>
+            <img src={logo} alt="Logo"/>
             <div className="buttons-wrapper">
                 <button>Sign In</button>
                 <div>- OR -</div>
