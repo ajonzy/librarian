81c212fdaff135a223b3633c8f5cafb3232d6db8
alexjones <ajonzy@gmail.com>
2021-09-04 14:20:04 -0600

    Cleaned up book view

--- src/components/pages/pages/book/book.js
+++ src/components/pages/pages/book/book.js
@@ -21,14 +21,14 @@ export default function book({ title, author, published_year, number_of_pages, t
         <div className="book">
             <button onClick={() => handleViewBookCancel()}>Back</button>
             <h2>{title}</h2>
+            <h5>By</h5>
             <h3>{author}</h3>
-            <p>Published: {published_year}</p>
+            <p>Published: {published_year ? published_year : "Unknown"}</p>
             <p>Pages: {number_of_pages ? number_of_pages : "Unknown"}</p>
-            <img src={thumbnail_url} alt=""/>
             <p>Read: <input type="checkbox" checked={read} disabled /></p>
-            {/* TODO: Add grey stars to represent negative ratings */}
-            <p>Rating: {rating ? "\u2605".repeat(rating) : "No Rating"}</p>
-            <p>Notes: {notes}</p>
+            {thumbnail_url ? <img src={thumbnail_url} alt=""/> : null}
+            {rating ? <p>Rating: {"\u2605".repeat(rating)}<span style={{ color: "grey" }}>{"\u2605".repeat(10 - rating)}</span></p> : null}
+            {notes !== "" ? <p style={{ whiteSpace: "pre-line" }}>Notes: {notes}</p> : null}
             {series_data ? <h3>Series</h3> : null}
             {series_data ? <h4>{series_data.name}</h4> : null}
             {series_data ? renderSeries() : null}
