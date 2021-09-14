eb1d279818d6cce1c14698c06cde3cef1b7930e4
Alex Jones <ajonzy@gmail.com>
2021-09-13 21:07:47 -0600

    Autosuggest styles

--- src/components/utitlities/autosuggest.js
+++ src/components/utitlities/autosuggest.js
@@ -8,7 +8,7 @@ export default function autosuggest(props) {
         const inputValue = value.trim().toLowerCase();
         const inputLength = inputValue.length
 
-        return inputLength === 0 ? [] : props.suggestions.filter(suggestion =>
+        return inputLength === 0 ? props.suggestions : props.suggestions.filter(suggestion =>
             suggestion.toLowerCase().includes(inputValue)
         )
     }
@@ -20,9 +20,11 @@ export default function autosuggest(props) {
             onSuggestionsClearRequested={() => setInputSuggestions([])}
             getSuggestionValue={suggestion => suggestion}
             renderSuggestion={suggestion => <div>{suggestion}</div>}
+            alwaysRenderSuggestions={true}
             inputProps={{
                 placeholder: props.placeholder,
                 value: props.input,
+                onFocus: () => getSuggestions(""),
                 onChange: (event, { newValue }) => props.setInput(newValue)
             }}
         />
